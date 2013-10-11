b3bdf923ad76448c123d1cde7540663f95b0a7bf
Prateek <prateekredd@gmail.com>
2013-10-11 00:35:13 -0700

    Added Functionality and bug fixes

--- rip_router.py
+++ rip_router.py
@@ -9,8 +9,7 @@ class RIPRouter (Entity):
         # Add your code here!
         self.distance_vector = DistanceVector(self)
         self.neighbor_ports = {}
-        self.routing_packet = None
- 
+         
     def handle_rx (self, packet, port):
         # Add your code here!
         if type(packet) is DiscoveryPacket:
@@ -20,7 +19,7 @@ class RIPRouter (Entity):
                     self.send_dv_update()
             elif not packet.is_link_up or packet.latency >= 100:
                 if self.distance_vector.delete_node(packet.src):
-                    del(self.distance_vector[port])
+                    del(self.neighbor_ports[packet.src])
                     self.send_dv_update()
                 
         elif type(packet) is RoutingUpdate:
@@ -28,8 +27,8 @@ class RIPRouter (Entity):
     
     def send_dv_update(self):
         for neighbor in self.neighbor_ports.keys():
-            self.routing_packet = self.distance_vector.get_routing_packet(neighbor)
-            self.send(self.routing_packet, None, True)
+            routing_packet = self.distance_vector.get_routing_packet(neighbor)
+            self.send(routing_packet, None, True)
             
 class DistanceVector ():
     def __init__(self, owner):
@@ -38,42 +37,66 @@ class DistanceVector ():
         
     def update_from_packet(self, update_packet):
         update_required = False
-        for dest in update_packet.all_dests():
-            if dest == self.owner: continue
+        for dest in list(set(self.dest_via_nbors.keys()) | set(update_packet.all_dests())):
             oldClosestDist = self.distance_to(dest)
+            if update_packet.src == dest or self.owner == dest:
+                continue
+            elif not dest in update_packet.all_dests():
+                del(self.dest_via_nbors[dest][update_packet.src])
+                if len(self.dest_via_nbors[dest].keys()) == 0: del(self.dest_via_nbors[dest])
+            elif not dest in self.dest_via_nbors:
+                self.update_vector(dest, update_packet.src, update_packet.get_distance(dest))
             self.update_vector(dest, update_packet.src, update_packet.get_distance(dest))
-            update_required = update_required or oldClosestDist > self.distance_to(dest)
+            update_required = update_required or oldClosestDist != self.distance_to(dest)
         return update_required
             
     def update_vector(self, dest, neighbor, dist_from_neighbor):
-        update_required = (not dest in self.dest_via_nbors) or (dist_from_neighbor < self.distance_to(dest))
+        old_distance = self.distance_to(dest)
         if not dest in self.dest_via_nbors: self.dest_via_nbors[dest] = {}
-        self.dest_via_nbors[dest][neighbor] = dist_from_neighbor
-        return update_required
+        if dist_from_neighbor >= 99:
+            if neighbor in self.dest_via_nbors:
+                del(self.dest_via_nbors[dest][neighbor])
+                if len(self.dest_via_nbors) == 0: del(self.dest_via_nbor[dest][neighbor])
+        else:
+            self.dest_via_nbors[dest][neighbor] = dist_from_neighbor
+        return old_distance != self.distance_to(dest)
     
-    def delete_node(self, node):
+    def delete_link(self, node):
         update_required = False
         for dest in self.dest_via_nbors.keys():
-            if dest == node:
-                del(self.dest_via_nbors[dest])
+            oldDist = self.distance_to(dest)
+            if dest == node and None in self.dest_via_nbors[dest]:
+                del(self.dest_via_nbors[dest][None])
+                if len(self.dest_via_nbors[dest].keys()) == 0: del(self.dest_via_nbors[dest])
                 update_required = True
-                continue
             else:
                 if node in self.dest_via_nbors[dest]:
-                    oldDist = self.distance_to(dest)
                     del(self.dest_via_nbors[dest][node])
-                    if oldDist != self.distance_to(dest): update_required = True
+            if oldDist != self.distance_to(dest): update_required = True
         return update_required      
     
     def get_routing_packet(self,packet_dst):
         routing_packet = RoutingUpdate()
+        routing_packet.src = self.owner
         for dest in self.dest_via_nbors.keys():
+            if dest not in self.owner.neighbor_ports.keys() and self.closestNeighborTo(dest) == packet_dst:
+                continue
             routing_packet.add_destination(dest, self.distance_to(dest))
         return routing_packet
     
     def distance_to(self, dest):
         if not dest in self.dest_via_nbors: return float("inf")
         return min(self.dest_via_nbors[dest].values()) + 1
+    
+    def closestNeighborTo(self, dest):
+        distance = self.distance_to(dest)
+        if distance >= 100: return None
+        closestNeighbors = {}
+        for neighbor in self.dest_via_nbors[dest].keys():
+            if distance == self.dest_via_nbors[dest][neighbor]:
+                closestNeighbors[self.owner.neighbor_ports[neighbor]] = neighbor
+        return closestNeighbors[min(closestNeighbors.values())]
+    
     
     def get_neighbors(self):
         neighbors = []
--- tests/RipRouterTests.py
+++ tests/RipRouterTests.py
@@ -56,9 +56,9 @@ class Test(unittest.TestCase):
         r5 = RIPRouter()
         r5.name = "r5"
         
-        r3.handle_rx(DiscoveryPacket(r1,0), None)
-        r3.handle_rx(DiscoveryPacket(r4,0), None)
-        r3.handle_rx(DiscoveryPacket(r5,0), None)
+        r3.handle_rx(DiscoveryPacket(r1,1), None)
+        r3.handle_rx(DiscoveryPacket(r4,4), None)
+        r3.handle_rx(DiscoveryPacket(r5,5), None)
         
         packetOne = RoutingUpdate()
         packetOne.src = r1
@@ -70,9 +70,9 @@ class Test(unittest.TestCase):
         packetFive.src = r5
         packetFive.paths = {r1:2,r2:2,r3:1,r4:1}
         
-        r3.handle_rx(packetOne, 0)
-        r3.handle_rx(packetFour, 0)
-        r3.handle_rx(packetFive, 0)
+        r3.handle_rx(packetOne, 1)
+        r3.handle_rx(packetFour, 4)
+        r3.handle_rx(packetFive, 5)
 
         r3_table = {r1:{None:0,r4:2,r5:2},r2:{r1:1,r4:1,r5:2},r4:{r1:2,None:0,r5:1},r5:{r1:2,r4:1,None:0}}
         self.failUnless(r3.distance_vector.dest_via_nbors == r3_table)
@@ -89,9 +89,43 @@ class Test(unittest.TestCase):
         r5 = RIPRouter()
         r5.name = "r5"
         
-        r5.handle_rx(DiscoveryPacket(r3,0), None)
-        r5.handle_rx(DiscoveryPacket(r4,0), None)
+        r5.handle_rx(DiscoveryPacket(r3,0), 3)
+        r5.handle_rx(DiscoveryPacket(r4,0), 4)
         
+        packetThree = RoutingUpdate()
+        packetThree.src = r3
+        packetThree.paths = {r1:1,r2:2,r4:1,r5:1}
+        r5.handle_rx(packetThree, 3)
+        packetFour = RoutingUpdate()
+        packetFour.src = r4
+        packetFour.paths = {r1:2,r2:1,r3:1,r5:1}
+        r5.handle_rx(packetFour, 4)
+        
+        r5_table = {r1:{r3:1,r4:2},r2:{r3:2,r4:1},r3:{None:0,r4:1},r4:{r3:1,None:0}}
+        self.failUnless(r5.distance_vector.dest_via_nbors == r5_table)
+        self.failUnless(r5.neighbor_ports == {r3:0,r4:0})
+        self.failUnless(r5.distance_vector.delete_link(r3))
+        r5_table = {r1:{r4:2},r2:{r4:1},r3:{r4:1},r4:{None:0}}
+        self.failUnless(r5.distance_vector.dest_via_nbors == r5_table)
+
+    """
+    r1 -- r2 -- r3
+    """
+    def testNaivePoisonReverse(self):
+        r1 = RIPRouter()
+        r1.name = "r1"
+        r2 = RIPRouter()
+        r2.name = "r2"
+        r3 = RIPRouter()
+        r3.name = "r3"
+        
+        r1.handle_rx(DiscoveryPacket(r2,0),0)
+        packetOne = RoutingUpdate
+        packetOne.paths = {r1:1, r3:1}
+        packetOne.src = r2
+        r1.handle_rx(RoutingUpdate, 0)
+        
+        self.failUnless(r1.distance_vector.get_routing_packet(r2).paths == {r2:1})
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
